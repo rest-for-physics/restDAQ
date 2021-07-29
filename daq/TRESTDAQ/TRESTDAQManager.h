6f50b74104ed47d085d261c86eecab039a80f5d7
juanan <juanan318@gmail.com>
2021-07-29 16:17:16 +0200

    New TRESTDAQManager for data acquisition

---
+++ daq/TRESTDAQ/TRESTDAQManager.h
@@ -0,0 +1,62 @@
+/*********************************************************************************
+TRESTDAQManager.h
+
+Manager for TRESTDAQ
+
+Author: JuanAn Garcia 14/07/2021
+
+Control data acquisition via shared memory, should be always running
+
+*********************************************************************************/
+
+#ifndef __TREST_DAQ_MANAGER__
+#define __TREST_DAQ_MANAGER__
+
+#include <iostream>
+#include <string>
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#include "TRESTDAQ.h"
+
+class TRESTDAQManager {
+   public:
+    TRESTDAQManager( );
+    ~TRESTDAQManager( );
+
+    void run();
+
+    //Shared memory
+    struct sharedMemoryStruct{
+      char cfgFile[1024];
+      char runType[256];
+      char runName[1024];
+      int startDAQ;
+      int status;
+      int eventCount;
+      int nEvents;
+      int exitManager;
+      int abortRun;
+    };
+
+    void dataTaking( );
+
+    //Shared Memory
+    static void InitializeSharedMemory(sharedMemoryStruct *sM);
+    static void PrintSharedMemory(sharedMemoryStruct *sM);
+    static bool GetSharedMemory(int &sid, sharedMemoryStruct **sM, int flag);
+    static void DetachSharedMemory(sharedMemoryStruct **sM);
+
+    //Control commands
+    static void StopRun( );
+    static void ExitManager( );
+    static void AbortThread( );
+
+   private:
+    inline static const key_t key {0x12367};
+
+};
+
+#endif
+
