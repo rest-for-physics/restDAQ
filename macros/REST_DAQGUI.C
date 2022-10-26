a06714d84315ae3643cfa4727131e51c976f6578
Luis Obis <luis.antonio.obis@gmail.com>
2022-10-26 19:56:44 +0200

    remove macros

--- macros/REST_DAQGUI.C
+++
@@ -1,9 +0,0 @@
-
-#include "TRestDAQGUI.h"
-
-void REST_DAQGUI(const std::string &inputRMLFile) {
-
-  TRestDAQGUIMetadata *mt = new TRestDAQGUIMetadata(inputRMLFile.c_str());
-  new TRestDAQGUI(1000, 700, mt); 
- 
-}
